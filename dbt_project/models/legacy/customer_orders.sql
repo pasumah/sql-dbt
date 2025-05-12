70d5403780f55cb4dd6786eced856670937c3e0e
Mahesh Pasupuleti <maheshpasupuleti@Maheshs-MacBook-Pro.local>
2025-05-12 00:57:00 -0600

    Initial commit to dbt demo

---
+++ dbt_project/models/legacy/customer_orders.sql
@@ -0,0 +1,24 @@
+WITH customer_orders AS (
+    SELECT
+        customer_id,
+        MIN(order_date) AS first_order_date,
+        MAX(order_date) AS most_recent_order_date,
+        COUNT(order_id) AS number_of_orders
+    FROM
+        jaffle_shop.orders
+    GROUP BY
+        1
+)
+SELECT
+    customers.customer_id,
+    customers.first_name,
+    customers.last_name,
+    customer_orders.first_order_date,
+    customer_orders.most_recent_order_date,
+    COALESCE(
+        customer_orders.number_of_orders,
+        0
+    ) AS number_of_orders
+FROM
+    jaffle_shop.customers
+    LEFT JOIN customer_orders USING (customer_id)
